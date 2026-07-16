3e2334911ee0fb2153df97f357ba8e6d905b02da
ilpanich <ilpanich@users.noreply.github.com>
2026-07-16 10:41:38 +0000

    docs(server): publish rustdoc for axiam-server/v1.0.0-alpha1 520b31b2c12d053738bc9c853901c3a444f4a3e7

--- server/trait.impl/axiam_core/repository/trait.MailPublisher.js
+++ server/trait.impl/axiam_core/repository/trait.MailPublisher.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["axiam_amqp",[["impl <a class=\"trait\" href=\"axiam_core/repository/trait.MailPublisher.html\" title=\"trait axiam_core::repository::MailPublisher\">MailPublisher</a> for <a class=\"struct\" href=\"axiam_amqp/mail_publisher/struct.MailOutboundPublisher.html\" title=\"struct axiam_amqp::mail_publisher::MailOutboundPublisher\">MailOutboundPublisher</a>",0]]]]);
+    const implementors = Object.fromEntries([["axiam_amqp",[["impl MailPublisher for <a class=\"struct\" href=\"axiam_amqp/mail_publisher/struct.MailOutboundPublisher.html\" title=\"struct axiam_amqp::mail_publisher::MailOutboundPublisher\">MailOutboundPublisher</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[360]}
+//{"start":59,"fragment_lengths":[227]}
